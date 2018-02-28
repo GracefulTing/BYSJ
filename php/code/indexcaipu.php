80cc6ecddd0a806e38723aca0649e854f1094ff8
GracefulTing <32625852+GracefulTing@users.noreply.github.com>
2018-02-28 22:07:53 +0800

    update

---
+++ php/code/indexcaipu.php
@@ -0,0 +1,17 @@
+<?php 
+	header('Content-Type:text/html;charset=utf-8');
+	echo '[
+		{"img":"../php/img/glbm.jpg","title":"咖喱拌面"},
+		{"img":"../php/img/jby.jpg","title":"三葱焗鲍鱼"},
+		{"img":"../php/img/cyjc.jpg","title":"豉油鸡翅"},
+		{"img":"../php/img/gqrs.jpg","title":"枸杞肉丝"},
+		{"img":"../php/img/qjme.jpg","title":"青椒木耳炒肉片"},
+		{"img":"../php/img/c1.jpg","title":"烤箱版秘制鸡腿饭"},
+		{"img":"../php/img/fmnm.jpg","title":"蜂蜜柠檬茶"},
+		{"img":"../php/img/hdym.jpg","title":"红豆燕麦牛奶粥"},
+		{"img":"../php/img/lyt.jpg","title":"滋补老鸭汤"},
+		{"img":"../php/img/ymz.jpg","title":"薏米粥"},
+		{"img":"../php/img/ycsy.jpg","title":"鱼翅鲨鱼骨汤"},
+		{"img":"../php/img/yt.jpg","title":"樱桃酒"}
+	]';
+?>
