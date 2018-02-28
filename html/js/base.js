869fadd384ab7a394cb455bf4c807c737c6de4d4
GracefulTing <32625852+GracefulTing@users.noreply.github.com>
2018-02-28 21:37:14 +0800

    add

---
+++ html/js/base.js
@@ -0,0 +1,6 @@
+//创建元素的函数
+// function createEle(ele,father,className){
+// 	var n = $("<" + ele + " class=" + className +">");
+// 	n.appendTo(father);
+// 	return n;
+// }
---
+++ php/code/caipu.php
@@ -0,0 +1,13 @@
+<?php 
+	header('Content-Type:text/html;charset=utf-8');
+	echo '[
+		{"title":"日本嫩牛肉","img":"../php/img/japan.jpg","rq":"9104"},
+		{"title":"芹菜炒牛肉","img":"../php/img/japan.jpg","rq":"859"},
+		{"title":"秘制酱香豆腐","img":"../php/img/japan.jpg","rq":"1104"},
+		{"title":"萝卜丝肉丸","img":"../php/img/japan.jpg","rq":"3029"},
+		{"title":"萝卜牛肉堡","img":"../php/img/japan.jpg","rq":"2344"},
+		{"title":"烤箱版蜜汁鸡腿饭","img":"../php/img/japan.jpg","rq":"1959"},
+		{"title":"干炒排骨","img":"../php/img/japan.jpg","rq":"5094"},
+		{"title":"黄豆肉末炒咸菜","img":"../php/img/japan.jpg","rq":"3023"}
+	]';
+?>
