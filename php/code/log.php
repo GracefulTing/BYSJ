159113db3e53f038fce146dcfcd4a2116fca2e5f
GracefulTing <953632795@qq.com>
2018-03-27 17:55:06 +0800

    update file

--- php/code/log.php
+++ php/code/log.php
@@ -19,9 +19,19 @@ else if($password == ""){
     $resource=mysqli_query($link,$sql);
     $colum1= mysqli_fetch_assoc($resource); 
     mysqli_close($link);
+    // if(($colum1['uid'] == $username) && ($colum1['psw1'] == $password)){    
+    //     echo"<script type='text/javascript'>alert('登陆成功');window.location.href='../../html/index.html';</script>"; 
+    // }else    
+    //     echo"<script type='text/javascript'>alert('密码错误');location='../../html/log.html';</script>";   
+    // }  
+    if($colum1['gender'] == '女'){
+        echo"<script type='text/javascript'>sessionStorage.setItem('sex','女');</script>"; 
+    }else{
+        echo"<script type='text/javascript'>sessionStorage.setItem('sex','男');</script>"; 
+    }
     if(($colum1['uid'] == $username) && ($colum1['psw1'] == $password)){    
-        echo"<script type='text/javascript'>alert('登陆成功');window.location.href='../../html/index.html';</script>"; 
+        echo"<script type='text/javascript'>alert('登陆成功');window.location.href='../../html/index.html';sessionStorage.setItem('state','$username');</script>"; 
     }else    
         echo"<script type='text/javascript'>alert('密码错误');location='../../html/log.html';</script>";   
-    }  
+    } 
 ?>  
