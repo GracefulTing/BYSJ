7a4af7d35b3ce838a34580e5e30a806b5bf0bf75
GracefulTing <953632795@qq.com>
2018-03-28 11:43:00 +0800

    update index link

--- php/code/indexcaipu.php
+++ php/code/indexcaipu.php
@@ -1,17 +1,17 @@
 <?php 
 	header('Content-Type:text/html;charset=utf-8');
 	echo '[
-		{"img":"../php/img/glbm.jpg","title":"咖喱拌面"},
-		{"img":"../php/img/jby.jpg","title":"三葱焗鲍鱼"},
-		{"img":"../php/img/tchs.jpg","title":"糖醋红薯片"},
-		{"img":"../php/img/gqrs.jpg","title":"枸杞肉丝"},
-		{"img":"../php/img/qjme.jpg","title":"青椒木耳炒肉片"},
-		{"img":"../php/img/c1.jpg","title":"烤箱版秘制鸡腿饭"},
-		{"img":"../php/img/fmnm.jpg","title":"蜂蜜柠檬茶"},
-		{"img":"../php/img/hdym.jpg","title":"红豆燕麦牛奶粥"},
-		{"img":"../php/img/fsyct.jpg","title":"粉丝油菜汤"},
-		{"img":"../php/img/ymz.jpg","title":"薏米粥"},
-		{"img":"../php/img/xhsym.jpg","title":"西红柿燕麦粥"},
-		{"img":"../php/img/yt.jpg","title":"樱桃酒"}
+		{"img":"../php/img/glbm.jpg","title":"咖喱拌面","link":"http://www.cnys.com/caipu/jiachangcai/90072.html"},
+		{"img":"../php/img/jby.jpg","title":"三葱焗鲍鱼","link":"http://www.cnys.com/caipu/jiachangcai/89346.html"},
+		{"img":"../php/img/tchs.jpg","title":"糖醋红薯片","link":"http://www.cnys.com/caipu/cai/85649.html"},
+		{"img":"../php/img/gqrs.jpg","title":"枸杞肉丝","link":"http://www.cnys.com/caipu/cai/34371.html"},
+		{"img":"../php/img/qjme.jpg","title":"青椒木耳炒肉片","link":"http://www.cnys.com/caipu/cai/85139.html"},
+		{"img":"../php/img/c1.jpg","title":"烤箱版秘制鸡腿饭","link":"http://www.cnys.com/caipu/jiachangcai/90027.html"},
+		{"img":"../php/img/fmnm.jpg","title":"蜂蜜柠檬茶","link":"http://www.cnys.com/caipu/cha/88700.html"},
+		{"img":"../php/img/hdym.jpg","title":"红豆燕麦牛奶粥","link":"http://www.cnys.com/caipu/zhou/34085.html"},
+		{"img":"../php/img/fsyct.jpg","title":"粉丝油菜汤","link":"http://www.cnys.com/caipu/tang/35728.html"},
+		{"img":"../php/img/ymz.jpg","title":"薏米粥","link":"http://www.cnys.com/caipu/zhou/36006.html"},
+		{"img":"../php/img/xhsym.jpg","title":"西红柿燕麦粥","link":"http://www.cnys.com/caipu/zhou/87970.html"},
+		{"img":"../php/img/yt.jpg","title":"樱桃酒","link":"http://www.cnys.com/caipu/jiu/33956.html"}
 	]';
 ?>
